11e9012cd3151bcf5f08670c18a95bf63df65d39
Allan Santos <alnsantos90@gmail.com>
2020-05-16 20:43:54 -0300

    Criação página ideias.html, correção Modal

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,16 @@
 // OPEN MODAL
 function onOff() {
   const getModal = document.getElementById('modal')
+  const getBody = document.querySelector('body')
     getModal
       .classList
       .toggle('hide')
+
+    getBody
+      .classList
+      .toggle('hideScroll')
+
+    getModal
+      .classList
+      .toggle('addScroll')
 }
