1ee27225ff3fbe38c8b10faa497c3d9b9e1d1054
Allan Santos <alnsantos90@gmail.com>
2020-05-30 15:57:42 -0300

    Server.js, atualizado

--- server.js
+++ server.js
@@ -3,20 +3,18 @@ const express = require('express');
 const server = express()
 const nunjucks = require('nunjucks');
 const db = require('./db');
-
 server.use(express.static("public"))
-server.use(express.urlencoded({ extended: true }))
+server.use(express.urlencoded({
+  extended: true
+}))
 nunjucks.configure('views', {
   express: server,
   noCache: true
 })
-
-
 // PAGE DAFAULT
 server.get('/', (req, res) => {
   db.all(`SELECT * FROM ideas`, (err, rows) => {
     if (err) return console.log(err);
-
     const lastIdeas = []
     const reverserdIdeas = [...rows].reverse()
     for (idea of reverserdIdeas) {
@@ -29,7 +27,6 @@ server.get('/', (req, res) => {
     })
   })
 })
-
 // LIST IDEAS
 server.get('/ideas', (req, res) => {
   db.all(`SELECT * FROM ideas`, (err, rows) => {
@@ -39,33 +36,31 @@ server.get('/ideas', (req, res) => {
     })
   })
 })
-
 // CREATE IDEA
 server.post('/', (req, res) => {
-  const { title, image, category, description, url } = req.body
-
+  const {
+    title,
+    image,
+    category,
+    description,
+    url
+  } = req.body
   db.run(`INSERT INTO ideas (title, image, category, description, url)
   VALUES (?, ?, ?, ?, ?)`, [title, image, category, description, url], (err) => {
-    if(err) {
+    if (err) {
       res.send('Erro de conexão no banco de dados.')
     }
     res.redirect('/ideas')
   })
-
 })
-
 // DELETE IDEA
 server.post('/delete/:id', (req, res) => {
   const idIdea = req.params.id
-
   db.run(`DELETE FROM ideas WHERE id = ?`, [idIdea], (err) => {
-    if(err) console.log(err);
-
+    if (err) console.log(err);
     res.redirect('/ideas')
   })
-
 })
-
 server.listen(port, () => {
   console.log(`Servidor ON, rodando em http://localhost:${port}`);
 })
