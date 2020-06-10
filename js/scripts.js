af2c584e7f0d46853833f7d01ffad21dc257dcd0
Allan Santos <allansantos@Allans-MacBook-Pro.local>
2020-06-10 00:21:42 -0300

    Alteração responsive.css (menu toggle)

--- js/scripts.js
+++ js/scripts.js
@@ -16,37 +16,65 @@ function onOff() {
 }
 
 
+
+
 // Toggle - Menu
 const getBtnToggle = document.querySelector('.menu-mobile')
 const getNav = document.querySelector('.menu-mobile nav')
+const getTitle = document.querySelector('#title')
+const getMain = document.querySelector('main')
 
-let toggle = false
+
+// TIMELINE TOGGLE
+const tl = new TimelineMax({
+  paused: true
+})
+
+tl.staggerTo(getMain, 0.1, {
+  y: 0
+})
+tl.staggerTo(getTitle, 0.1, {
+  y: 0
+},0, '-=.1')
+tl.staggerTo(getBtnToggle, 0, {
+  x: 0
+})
+tl.staggerTo(getNav, 0, {
+  visibility: 'visible',
+  x: 0
+})
+
+
+
+// FUNÇÃO SHOW AND CLOSE TOGGLE
+let toggle = true
 function menuToogle() {
-  
-  if (!toggle) {
-    TweenMax.set(getNav, {display: 'none', x: -300})
-    TweenMax.to(getNav, .3, {
-      display: "block",
-      x: 0
-    })
-    TweenMax.to(getBtnToggle, 0.1, {
-      autoAlpha: 1,
-      display: "block"
-    })
-    toggle = true
-
+  if (toggle) {
+    tl.play()
+    toggle = false
 
   } else {
-    toggle = false
-    TweenMax.to(getNav, .3, {
-      display: 'none',
-      x: -300
-    })
-
-    TweenMax.to(getBtnToggle, .2, {
-      autoAlpha: 0,
-      display: 'none',
-    })
+    tl.reverse()
+    toggle = true
   }
-
 }
+
+
+
+
+
+window.onresize = () => {
+    if(window.innerWidth >= 601) {
+      getNav.style.visibility = 'visible'
+      getNav.style.transform = 'translateX(0px)'
+      getBtnToggle.style.transform = 'translateX(0px)'
+      getTitle.style.transform = 'translateY(0px)'
+      getMain.style.transform = 'translateY(0px)'
+    } else {
+      getNav.style.visibility = 'hidden'
+      getNav.style.transform = 'translateX(500px)'
+      getBtnToggle.style.transform = 'translateX(500px)'
+      getTitle.style.transform = 'translateY(-80px)'
+      getMain.style.transform = 'translateY(-80px)'
+    }
+  }
